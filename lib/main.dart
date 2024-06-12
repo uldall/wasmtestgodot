dbfefdf791c6bcf38514c1ef2798e0a0c1a19a06
Christian Uldall Pedersen <cup@gamblify.com>
2024-06-12 18:15:04 +0200

    Initial version.

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_web/webview_flutter_web.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  WebViewController controller = WebViewController();
+
+  _MyHomePageState() {
+    if (WebViewPlatform.instance is WebWebViewPlatform) {
+      final WebWebViewController webWebController =
+          controller.platform as WebWebViewController;
+
+      webWebController.setIFrameCredentialless(true);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: WebViewWidget(
+          controller: controller
+            ..loadRequest(Uri.parse(
+                'https://godotengine.github.io/godot-demo-projects/2d/bullet_shower/'))),
+    );
+  }
+}
